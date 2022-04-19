0669c920952971dc3cd863a5906db9018f25e111
mconiaris <mconiaris@gmail.com>
2022-04-19 10:47:30 -0400

    Created Welcome controller.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def code
+  end
+
+  def photography
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/code'
+  get 'welcome/photography'
+  get 'welcome/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
