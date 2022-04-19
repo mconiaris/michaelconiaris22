59d77a1c91830fe09ad8939f504a4b334d115576
mconiaris <mconiaris@gmail.com>
2022-04-19 10:57:54 -0400

    Commented out routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   root "welcome#index"
 
   get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/code'
-  get 'welcome/photography'
-  get 'welcome/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'welcome/about'
+  # get 'welcome/code'
+  # get 'welcome/photography'
+  # get 'welcome/contact'
 
 end
